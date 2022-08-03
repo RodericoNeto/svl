6fd4ce222d21d2c492587ccb4fc83d7a0fe65f34
RodericoNeto <rodericocrneto@gmail.com>
2022-08-02 21:02:58 -0300

    incluindo o select

--- src/components/FormBook.jsx
+++ src/components/FormBook.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
@@ -7,6 +7,7 @@ import Row from 'react-bootstrap/Row';
 
 export function FormBook() {
   const [validated, setValidated] = useState(false);
+  let [estados, setEstados] = useState([]);
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -18,46 +19,50 @@ export function FormBook() {
     setValidated(true);
   };
 
+  const fetchEstados = () => {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`)
+    .then(response => response.json())
+    .then(data => {
+      // console.log(`estados`, data)
+      setEstados(data);
+    })
+  }
+
+  useEffect(() => {
+    console.log(`useEffect`)
+    fetchEstados();
+  }, [])
+
   return (
+    <>
+    {console.log(`return`)}
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Row className="mb-3">
+
         <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>Título</Form.Label>
+          <Form.Label className='glitch'>Título</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="Título"
-            // defaultValue="Mark"
+          // defaultValue="Mark"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
+
         <Form.Group as={Col} md="4" controlId="validationCustom02">
           <Form.Label>Autor</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="Autor"
-            // defaultValue="Otto"
+          // defaultValue="Otto"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-        {/* <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-            <Form.Control
-              type="text"
-              placeholder="Username"
-              aria-describedby="inputGroupPrepend"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please choose a username.
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group> */}
       </Row>
       <Row className="mb-3">
+
         <Form.Group as={Col} md="6" controlId="validationCustom03">
           <Form.Label>City</Form.Label>
           <Form.Control type="text" placeholder="City" required />
@@ -65,13 +70,15 @@ export function FormBook() {
             Please provide a valid city.
           </Form.Control.Feedback>
         </Form.Group>
+
         <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
+          <Form.Label>Estado</Form.Label>
+          <Form.Select aria-label="Default select example">
+            <option >Selecione o estado...</option>
+            {console.log(`estados`, estados)}
+          </Form.Select>
         </Form.Group>
+
         <Form.Group as={Col} md="3" controlId="validationCustom05">
           <Form.Label>Zip</Form.Label>
           <Form.Control type="text" placeholder="Zip" required />
@@ -79,9 +86,11 @@ export function FormBook() {
             Please provide a valid zip.
           </Form.Control.Feedback>
         </Form.Group>
+
       </Row>
       <Button type="submit">Submit form</Button>
     </Form>
+    </>
   );
 }
 
